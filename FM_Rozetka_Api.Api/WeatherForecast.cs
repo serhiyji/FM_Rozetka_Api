9c3e2dddb975e6f2506b4c77bef3aa6638d3d3b2
SerhiyJI <serhiyji@gmail.com>
2024-05-15 21:25:33 +0300

    Add project files.

---
+++ FM_Rozetka_Api.Api/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace FM_Rozetka_Api.Api
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
