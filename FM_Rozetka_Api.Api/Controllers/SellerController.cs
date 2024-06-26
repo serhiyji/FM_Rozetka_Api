dbd170cb15185960339dfa03f9550f96d3f958b4
Pasha Mayba <pashamayba7@gmail.com>
2024-06-26 15:02:04 +0300

    A controller for receiving data is made

---
+++ FM_Rozetka_Api.Api/Controllers/SellerController.cs
@@ -0,0 +1,25 @@
+﻿using FM_Rozetka_Api.Core.DTOs;
+using FM_Rozetka_Api.Core.DTOs.Seller;
+using FM_Rozetka_Api.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FM_Rozetka_Api.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellerController : ControllerBase
+    {
+        public SellerController()
+        {
+            
+        }
+
+        [HttpPost("seller-start")]
+        public async Task<IActionResult> SellerStart([FromBody] SellerStartDTO model)
+        {
+            return Ok();
+        }
+    }
+}
