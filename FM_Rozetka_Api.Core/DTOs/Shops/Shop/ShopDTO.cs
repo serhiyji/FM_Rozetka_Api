ca493f9e26b46bb9fc2be921e05d456715c1dee4
Pasha Mayba <pashamayba7@gmail.com>
2024-07-29 12:45:32 +0300

    Done CRUD for Shop table

--- FM_Rozetka_Api.Core/DTOs/Shops/Shop/ShopDTO.cs
+++ FM_Rozetka_Api.Core/DTOs/Shops/Shop/ShopDTO.cs
@@ -11,5 +11,12 @@ namespace FM_Rozetka_Api.Core.DTOs.Shops.Shop
     {
         public int Id { get; set; }
         public string AppUserId { get; set; }
+        public string CompanyName { get; set; }
+        public string Website { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsNonResident { get; set; }
     }
 }
---
+++ FM_Rozetka_Api.Core/Interfaces/IShopService.cs
@@ -0,0 +1,20 @@
+﻿using FM_Rozetka_Api.Core.DTOs.Company;
+using FM_Rozetka_Api.Core.DTOs.Shops.Shop;
+using FM_Rozetka_Api.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FM_Rozetka_Api.Core.Interfaces
+{
+    public interface IShopService
+    {
+        Task<IEnumerable<ShopDTO>> GetAllAsync();
+        Task<ShopDTO> GetByIdAsync(int id);
+        Task<Shop> AddAsync(ShopCreateDTO model);
+        Task UpdateAsync(ShopUpdateDTO model);
+        Task DeleteAsync(int id);
+    }
+}
