757932b59b8a788bb02c3661bdc1ceb3555db52e
Pasha Mayba <pashamayba7@gmail.com>
2024-08-07 18:27:40 +0300

    Modification of crud category product

--- FM_Rozetka_Api.Core/DTOs/Products/Product/ProductDTO.cs
+++ FM_Rozetka_Api.Core/DTOs/Products/Product/ProductDTO.cs
@@ -9,6 +9,7 @@ namespace FM_Rozetka_Api.Core.DTOs.Products.Product
     public class ProductDTO
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public decimal Stars { get; set; }
